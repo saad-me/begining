284234debeb25e9532210b79e54d6514859021e4
saad-me <furrukhsaad@gmail.com>
2023-06-07 03:19:15 -0700

    fixed sorting machine

--- sorteingmachine.js
+++ sorteingmachine.js
@@ -1,7 +1,4 @@
-const numbers = [53, 93, 946, 74, 74];
-function compare(a,b,c,d,e,f,g,h){
-    return a - b;
-    }
+const numbers = [53, 93, 96, 84, 74];
     numbers.sort()
 console.log(numbers)
 
