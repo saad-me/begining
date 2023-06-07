9c9d0b1d95e7997268525788830a9b2d9101c3b2
saad-me <furrukhsaad@gmail.com>
2023-06-07 01:28:19 -0700

    factotial added

--- firstapp.js
+++ firstapp.js
@@ -1,7 +1,7 @@
-function calculator(operator,numb1,numb2,ans){
+function calculator(operator,numb1,numb2,ans,factorial,vari){
     numb1 = prompt('enter any number:')
     numb2 = prompt('enter any number:')
-   operator = prompt('enter your operator(+-/%x):')
+   operator = prompt('enter your operator(+-/%xf):')
    switch (operator){
        case '+'
       :ans  = numb1 + + + numb2
@@ -23,6 +23,18 @@ function calculator(operator,numb1,numb2,ans){
       :ans = numb1 * numb2
       console.log(ans);
       break;
+      case 'f'
+      :factorial=prompt('for factorial choose number1(1) or choose number2(2):')
+      if (factorial=1)
+      for (vari = 1;vari<=numb1;vari++){
+     factorial = factorial*vari;
+      }
+       else {
+        for (vari = 1;vari<=numb2;vari++)
+        factorial = 1
+        factorial =factorial * vari;
+    }
+    console.log(factorial);break;
       default
       :console.error('invalid operator');
       
